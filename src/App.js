20f4162dd40457635f85d62152c9eec8e3e1ffcd
Ray <xrayjemmy@gmail.com>
2020-03-08 18:05:53 +0800

    add animation

--- src/App.js
+++ src/App.js
@@ -2,15 +2,95 @@ import React from "react";
 //import logo from "./logo.svg";
 import "./App.css";
 import Circle from "./Circle";
-import { animated, useTransition } from 'react-spring';
+import { ProgressBar } from 'react-bootstrap'
+import 'animate.css'
 const liff = window.liff;
+
+const defaults = {
+  list: [
+    {
+      id: 100,
+      name: "5000元京东卡",
+      image: "1.png",
+      rank: 1,
+      percent: 3
+    },
+    {
+      id: 101,
+      name: "1000元京东卡",
+      image: "2.png",
+      rank: 2,
+      percent: 5
+    },
+    {
+      id: 102,
+      name: "100个比特币",
+      image: "3.png",
+      rank: 3,
+      percent: 2
+    },
+    {
+      id: 103,
+      name: "50元话费",
+      image: "4.png",
+      rank: 4,
+      percent: 49
+    },
+    {
+      id: 104,
+      name: "100元话费",
+      image: "5.png",
+      rank: 5,
+      percent: 30
+    },
+    {
+      id: 105,
+      name: "500个比特币",
+      image: "6.png",
+      rank: 6,
+      percent: 1
+    },
+    {
+      id: 106,
+      name: "500元京东卡",
+      image: "7.png",
+      rank: 7,
+      percent: 10
+    }
+  ],
+  outerCircle: {
+    color: "#df1e15"
+  },
+  innerCircle: {
+    color: "#f4ad26"
+  },
+  dots: ["#fbf0a9", "#fbb936"],
+  disk: [
+    "#ffb933",
+    "#ffe8b5",
+    "#ffb933",
+    "#ffd57c",
+    "#ffb933",
+    "#ffe8b5",
+    "#ffd57c"
+  ],
+  title: {
+    color: "#5c1e08",
+    font: "19px Arial"
+  }
+};
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.myLiffId = props.liffId;
     this.state = {
-      name: props.name
+      name: props.name,
+      resourceLoaded: false,
+      progress: 20,
+      prizeList: defaults,
+      prizeImages: [],
+      startPressed: false
     };
   }
 
@@ -21,27 +101,68 @@ class App extends React.Component {
       let profile = await liff.getProfile();
       let displayName = profile.displayName;
       this.setState({
-        name: displayName
+        name: displayName,
+        resourceLoaded: true
       });
     } catch (error) {
       console.log(error);
+      this.setState({
+        progress: 45
+      })
     }
+    let loadedImage = [];
+    for (const item of this.state.prizeList.list) {
+      let image = await this.loadImage(item.image);
+      loadedImage.push(image);
+    }
+    this.setState({
+      progress: 100,
+      prizeImages: loadedImage
+    })
   }
 
   initializeLiff(myLiffId) {
     return liff.init({ liffId: myLiffId });
   }
 
+  loadImage(imageSrc) {
+    return new Promise((resolve, reject) => {
+      let img = new Image();
+      img.src = imageSrc;
+      img.onerror = reject;
+      img.onload = () => {
+        resolve(img);
+      };
+    });
+  }
+
   render() {
+    let loading = this.state.progress <= 100 ? true : false;
+    let finishLoading = this.state.progress === 100 ? true : false
+    let introducing = this.state.resourceLoaded;
+    let started = this.state.startPressed;
+    let progressFadeOutAnimation = finishLoading ? "animated fadeOut delay-1s" : "";
+    let zoomIn = `animated zoomIn faster`
     return (
       <div className="App">
         <header className="App-header">
-          <Circle text="Raylmond" />
-          <div className="nes-container is-rounded is-centered">
-            <p>Welcome, {this.state.name}!</p>
-          </div>
-          <button className="nes-btn is-primary" variant="primary">Esun Lucky Wheelz</button>
-          <button type="button" className="nes-btn is-warning">Warning</button>
+          {loading && <ProgressBar
+            className={progressFadeOutAnimation}
+            onAnimationEnd={() => { this.setState({ resourceLoaded: true }) }}
+            style={{ width: `60%` }}
+            animated
+            now={this.state.progress}
+            label={`loading user info ${this.state.progress}%`}></ProgressBar>}
+          {introducing && !started && <button
+            className={`nes-btn is-primary ${zoomIn}`}
+            variant="primary">{`Esun Lucky Wheel for: ${this.state.name}`}</button>}
+          {introducing && !started && <button
+            type="button"
+            className={`nes-btn is-warning ${zoomIn}`}
+            onClick={() => this.setState({ startPressed: true })}>Warning</button>}
+          {started && <Circle
+            prizeImages={this.state.prizeImages}
+            defaults={this.state.prizeList} />}
         </header>
       </div>
     );
--- src/Circle.js
+++ src/Circle.js
@@ -1,86 +1,34 @@
 import React from "react";
 import BezierEasing from "bezier-easing";
+import styled from 'styled-components'
+import 'animate.css'
 
-const defaults = {
-  list: [
-    {
-      id: 100,
-      name: "5000元京东卡",
-      image: "1.png",
-      rank: 1,
-      percent: 3
-    },
-    {
-      id: 101,
-      name: "1000元京东卡",
-      image: "2.png",
-      rank: 2,
-      percent: 5
-    },
-    {
-      id: 102,
-      name: "100个比特币",
-      image: "3.png",
-      rank: 3,
-      percent: 2
-    },
-    {
-      id: 103,
-      name: "50元话费",
-      image: "4.png",
-      rank: 4,
-      percent: 49
-    },
-    {
-      id: 104,
-      name: "100元话费",
-      image: "5.png",
-      rank: 5,
-      percent: 30
-    },
-    {
-      id: 105,
-      name: "500个比特币",
-      image: "6.png",
-      rank: 6,
-      percent: 1
-    },
-    {
-      id: 106,
-      name: "500元京东卡",
-      image: "7.png",
-      rank: 7,
-      percent: 10
-    }
-  ],
-  outerCircle: {
-    color: "#df1e15"
-  },
-  innerCircle: {
-    color: "#f4ad26"
-  },
-  dots: ["#fbf0a9", "#fbb936"],
-  disk: [
-    "#ffb933",
-    "#ffe8b5",
-    "#ffb933",
-    "#ffd57c",
-    "#ffb933",
-    "#ffe8b5",
-    "#ffd57c"
-  ],
-  title: {
-    color: "#5c1e08",
-    font: "19px Arial"
-  }
-};
+const Wrapper = styled.div`
+  position: relative;
+  display: inline-blick;
+`;
+
+const StartButton = styled.img`
+  position: absolute;
+  top: 50%;
+  left:50%;
+  width:20%;
+  height:calc(20%*1.07);
+  margin-left: -10%;
+  margin-top: -10%;
+`;
 
 class Circle extends React.Component {
   constructor(props) {
     super(props);
     this.canvasRef = React.createRef();
     this.rotationDegree = 0;
-    this.state = {};
+    this.defaults = this.props.defaults;
+    this.prizeImages = this.props.prizeImages;
+    this.state = {
+      rotationDegree: 0,
+      leaving: false
+    };
   }
 
   drawCanvas(rotationDegree) {
@@ -91,7 +39,7 @@ class Circle extends React.Component {
     const height = canvas.height;
     ctx.clearRect(0, 0, width, height);
     let oneDegreeInRad = (2 * Math.PI) / 360;
-    let angel = oneDegreeInRad * (360 / defaults.list.length);
+    let angel = oneDegreeInRad * (360 / this.defaults.list.length);
     let startAngel = oneDegreeInRad * -90;
     let endAngel = oneDegreeInRad * -90 + angel;
     ctx.save();
@@ -100,17 +48,17 @@ class Circle extends React.Component {
 
     ctx.beginPath();
     ctx.lineWidth = 25;
-    ctx.strokeStyle = defaults.outerCircle.color;
+    ctx.strokeStyle = this.defaults.outerCircle.color;
     ctx.arc(0, 0, 243, 0, 2 * Math.PI);
     ctx.stroke();
 
     ctx.beginPath();
     ctx.lineWidth = 23;
-    ctx.strokeStyle = defaults.innerCircle.color;
+    ctx.strokeStyle = this.defaults.innerCircle.color;
     ctx.arc(0, 0, 218, 0, 2 * Math.PI);
     ctx.stroke();
 
-    let dotColors = defaults.dots;
+    let dotColors = this.defaults.dots;
     for (let i = 0; i < 12; i++) {
       ctx.beginPath();
       let radius = 230;
@@ -122,8 +70,8 @@ class Circle extends React.Component {
       startAngel += ((2 * Math.PI) / 360) * (360 / 12);
     }
 
-    let colors = defaults.disk;
-    for (let i = 0; i < defaults.list.length; i++) {
+    let colors = this.defaults.disk;
+    for (let i = 0; i < this.defaults.list.length; i++) {
       ctx.beginPath();
       ctx.lineWidth = 208;
       ctx.strokeStyle = colors[i % colors.length];
@@ -133,76 +81,36 @@ class Circle extends React.Component {
       endAngel += angel;
     }
     startAngel = angel / 2;
-    for (let i = 0; i < defaults.list.length; i++) {
+    for (let i = 0; i < this.defaults.list.length; i++) {
       ctx.save();
       ctx.rotate(startAngel);
-      ctx.drawImage(defaults.list[i].img, -48, -48 - 130);
-      ctx.fillStyle = defaults.title.color;
-      ctx.font = defaults.title.font;
+      ctx.drawImage(this.prizeImages[i], -48, -48 - 130);
+      ctx.fillStyle = this.defaults.title.color;
+      ctx.font = this.defaults.title.font;
       ctx.textAlign = "center";
-      ctx.fillText(defaults.list[i].name, 0, -170);
+      ctx.fillText(this.defaults.list[i].name, 0, -170);
       startAngel += angel;
       ctx.restore();
     }
     ctx.restore();
   }
 
-  loadImage(imageSrc) {
-    return new Promise((resolve, reject) => {
-      let img = new Image();
-      img.src = imageSrc;
-      img.onerror = reject;
-      img.onload = () => {
-        resolve(img);
-      };
-    });
-  }
-
   async componentDidMount() {
-    for (const item of defaults.list) {
-      item.img = await this.loadImage(item.image);
-    }
-    this.drawCanvas(this.rotationDegree);
+    this.drawCanvas(this.state.rotationDegree);
   }
 
-  componentDidUpdate() {
-    this.drawCanvas(this.rotationDegree);
+  componentDidUpdate(props) {
+    this.drawCanvas(this.state.rotationDegree);
   }
 
   handleClick() {
     const finalDegree = 15;
     const atLeastDegree = 2520;
     const rotation = finalDegree + atLeastDegree;
-    /*
-    //每秒60格，每16.6毫秒一格
-    //easeIn flatDuration easeOut 單位毫秒
-    //minSpeed maxSpeed 單位度/秒
-    const easeInMs = 500;
-    const flatMs = 2000;
-    const minSpeed = 0 * 0.016;
-    const maxSpeed = 600 * 0.016;
-    const easeInFrames = easeInMs * 0.06;
-    const flatFrames = flatMs * 0.06;
-    const easeOutFrames = (rotation - (maxSpeed * easeInFrames/3) - (maxSpeed * flatFrames)) * 1.5 / maxSpeed;
-    const easeFunction = this.genEaseFunction(
-      minSpeed,
-      maxSpeed,
-      easeInFrames,
-      flatFrames,
-      easeOutFrames
-    );
-    */
     let frames = 0;
     let angle = this.rotationDegree;
     const easeFunction = BezierEasing(0.07, -0.12, 0, 0.98);
     const update = () => {
-      /*
-      frames += 1;
-      if (frames > easeInFrames + easeOutFrames + flatFrames) {
-        console.log(angle);
-        return;
-      }
-      */
       frames = frames + 1;
       if (frames > 600) {
         this.rotationDegree = (angle + rotation) % 360;
@@ -210,48 +118,31 @@ class Circle extends React.Component {
         return;
       }
       let roDegree = easeFunction(frames / 600) * rotation;
-      this.drawCanvas((angle + roDegree) % 360);
+      this.setState({
+        rotationDegree: (angle + roDegree) % 360
+      })
       window.requestAnimationFrame(update);
     };
     window.requestAnimationFrame(update);
   }
 
-  genEaseFunction(min, max, easeInDuration, flatDuration, easeOutDuration) {
-    let startEaseOut = easeInDuration + flatDuration;
-    let end = easeInDuration + flatDuration + easeOutDuration;
-    let height = max - min;
-    return x => {
-      if (x <= easeInDuration) {
-        return (height * (x * x)) / (easeInDuration * easeInDuration) + min;
-      } else if (x > easeInDuration && x <= startEaseOut) {
-        return max;
-      } else if (x > startEaseOut && x <= end) {
-        return (
-          max -
-          (height * (x - startEaseOut) * (x - startEaseOut)) /
-            (easeOutDuration * easeOutDuration)
-        );
-      } else {
-        return min;
-      }
-    };
-  }
-
   render() {
+    let wrapperClass = 'animated zoomIn faster';
+    if (this.state.leaving) {
+      wrapperClass = 'animated bounceOutLeft'
+    }
+    this.defaults = this.props.defaults;
+    this.prizeImages = this.props.prizeImages;
     return (
-      <div className="lottery">
+      <Wrapper className={`${wrapperClass}`}>
         <canvas
           className="turntable-canvas"
           ref={this.canvasRef}
           width={600}
           height={600}
         />
-        <img
-          alt="start lottery"
-          src="start.png"
-          onClick={() => this.handleClick()}
-        ></img>
-      </div>
+        <StartButton alt="start" src="start.png" onClick={() => this.handleClick()}></StartButton>
+      </Wrapper>
     );
   }
 }
